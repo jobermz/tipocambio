8c3db9143fc114feb1c02484677e41d9ab58bd17
Jober Mena <jober.nena@gmail.com>
2021-03-07 17:03:52 -0500

    ok

---
+++ install-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mvn clean install
+docker build -t "tipocambio-docker" .
+docker run -d --name bbva-tipocambio-service -p 8080:8080 tipocambio-docker:latest
+docker container ls -a
+docker container stop bbva-tipocambio-service
+docker container start bbva-tipocambio-service
