6f238504722b5573b126034c43c50ee409de0fa4
jobermz <jober.mena@gmail.com>
2021-03-08 13:31:07 -0500

    Update install-docker.sh
    ok

--- install-docker.sh
+++ install-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mvn clean install
 docker build -t "tipocambio-docker" .
-docker run -d --name bbva-tipocambio-service -p 8080:8080 tipocambio-docker:latest
 docker container ls -a
 docker container rm bbva-tipocambio-service
-docker container stop bbva-tipocambio-service
-docker container start bbva-tipocambio-service
+docker run -d --name bbva-tipocambio-service -p 8080:8080 tipocambio-docker:latest
+#docker container stop bbva-tipocambio-service
+#docker container start bbva-tipocambio-service
